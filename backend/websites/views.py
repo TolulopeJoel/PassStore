b10287f396d10ce1c18dc98829d8340820e58e61
Tolu Joel <toluisjoel@gmail.com>
2023-02-28 12:21:09 +0100

    Added: - UserQuerySetMixin to Website and Credential views
    Refactored:
    - WebsiteViewset
    - CredentialViewset
    
    Creating of objects with seriaizer are now more efiicient.

--- backend/websites/views.py
+++ backend/websites/views.py
@@ -1,38 +1,30 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
+from accounts.mixins import UserQuerySetMixin
+
 from .models import Credential, Website
 from .serailizers import CredentialSerializer, WebsiteSerializer
 
 
-class WebsiteViewset(viewsets.ModelViewSet):
+class WebsiteViewset(UserQuerySetMixin, viewsets.ModelViewSet):
     queryset = Website.objects.all()
     serializer_class = WebsiteSerializer
-    
-    # def get_queryset(self):
-    #     queryset = Website.objects.filter(user=self.request.user)
-    #     return queryset
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
 
 
-class CredentialViewset(viewsets.ModelViewSet):
+class CredentialViewset(UserQuerySetMixin, viewsets.ModelViewSet):
     queryset = Credential.objects.all()
     serializer_class = CredentialSerializer
-    
-    def post(self, request, *args, **kwargs):
-        website_id = request.data.get('website_id')
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        website_id = self.request.data.get('website_id')
+
         try:
             website = Website.objects.get(id=website_id)
-        except Website.DoesNotExist:
+            return serializer.save(user=user, website=website)
+        except:
             return Response({'detail': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-        username = request.data.get('username')
-        password = request.data.get('password')
-        credential = Credential(
-            user=request.user,
-            website=website,
-            username=username,
-            password=password
-        )
-        credential.save()
-
-        return Response(CredentialSerializer(credential).data, status=status.HTTP_201_CREATED)
