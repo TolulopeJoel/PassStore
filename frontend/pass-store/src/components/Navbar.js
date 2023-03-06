606c516dd2b025b1f60cf7ea0b8c3bfeac5e6c02
Tolu Joel <toluisjoel@gmail.com>
2023-03-06 13:29:49 +0100

    Added NavBar component

---
+++ frontend/pass-store/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/"><b>Pass Store</b></a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="">SamePasswords</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="">Profile</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="">Logout</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="/login">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
