d1adaf3f4994b6e63de876f2b59d01511fd141b1
Tolu Joel <toluisjoel@gmail.com>
2023-03-06 13:19:03 +0100

    created pages folder and added WebsiteList

--- frontend/pass-store/src/App.js
+++ frontend/pass-store/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WebsiteList from './components/WebsiteList.js';
+import WebsiteList from './pages/WebsiteList.js';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 
