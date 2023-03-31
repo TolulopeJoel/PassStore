048729c314817e71e3d495e33cd8c835de8ecf35
Tolu Joel <toluisjoel@gmail.com>
2023-03-31 13:38:05 +0100

    return to home page after deleting credential

--- frontend/pass-store/src/pages/DeleteCredential.js
+++ frontend/pass-store/src/pages/DeleteCredential.js
@@ -1,42 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import api from "../components/api";
 
 
 function DeleteCredential() {
     const { credentialId } = useParams();
-    const [fieldErrors, setfieldErrors] = useState({});
-    const [otherErrors, setotherErrors] = useState({});
+    
+    const navigate = useNavigate()
 
     async function fetchData() {
         await api.delete(`/credentials/${credentialId}/`)
-            .then((response) => {
-            }).catch((error) => {
-                catchError(error);
-            });
-    }
-
-    function catchError(error) {
-        if (error.response.data.detail) {
-            setotherErrors([error.response.data.detail]);
-        } else if (error.response.data.message) {
-            setotherErrors([error.response.data.message]);
-        } else if (error.response.data.non_field_errors) {
-            setotherErrors(error.response.data.non_field_errors);
-        } else {
-            setfieldErrors(error.response.data);
-        }
     }
 
     useEffect(() => {
         fetchData()
+        navigate('/')
     }, [credentialId]);
-
-    return (
-        <>
-
-        </>
-    );
 }
 
 export default DeleteCredential;
