a759348effc9d6019b5cbbf1e030b54c30b5ef6b
Tolu Joel <toluisjoel@gmail.com>
2023-04-28 02:29:38 +0100

    Extended character limit for credential password field
    to allow encrypted  passwords to be stored.

--- backend/websites/models.py
+++ backend/websites/models.py
@@ -23,7 +23,7 @@ class Credential(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='credentials') 
     website = models.ForeignKey(Website, on_delete=models.CASCADE, related_name='credentials')
     username = models.CharField(max_length=50, blank=True)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=255)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
