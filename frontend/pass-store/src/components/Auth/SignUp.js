a69318e030b48e0dbc54e84fa61a5cfa4c2d2ce8
Tolu Joel <toluisjoel@gmail.com>
2023-05-07 14:57:11 +0100

    code refactoring

--- frontend/pass-store/src/components/Auth/SignUp.js
+++ frontend/pass-store/src/components/Auth/SignUp.js
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { useNavigate } from "react-router-dom";
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import api, { apiWithoutToken } from "../Api";
+import { apiWithoutToken } from "../Api";
 
 function Copyright(props) {
   return (
@@ -101,9 +97,6 @@ export default function SignUp() {
               <Grid item xs={12}>
                 {fieldErrors.password2 && (<div className="text-danger w-100">{fieldErrors.password2}</div>)}
                 <TextField required fullWidth label="Confirm Password" type="password" id="password2" value={password2} onChange={(event) => setPassword2(event.target.value)} />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel control={<Checkbox value="allowExtraEmails" color="primary" />} label="I want to receive inspiration and updates via email." />
               </Grid>
             </Grid>
             <Button type="submit" fullWidth variant="contained" className="bg-success bg-gradient" sx={{ mt: 3, mb: 2 }}> Sign Up </Button>
--- frontend/pass-store/src/pages/WebsiteList.js
+++ frontend/pass-store/src/pages/WebsiteList.js
@@ -48,47 +48,75 @@ export default function WebsiteList() {
 
                 <h1 className="display-6 my-5">Welcome { }</h1>
 
-                {websites.length > 0 && websites.map(website => (
-                    <div>
-                        <div className="row border border-2 align-items-center rounded-4 my-5 p-3">
-                            <h6 className="col-lg-8 col-md-8 col-sm-8"><a href={website.url}>{website.url}</a></h6>
-                            <h6 className="col-lg-2 col-md-2 col-sm-2">{website.credentials.length}</h6>
-                            <div className="col-lg-2 col-md-2 col-sm-2">
-                                <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={`#c-${website.id}`} role="button"
-                                    aria-expanded="false" aria-controls={`c-${website.id}`}> View
-                                </a>
-                            </div>
+                {websites.length > 0 ? (
+                    websites.map((website) => (
+                        <div key={website.id}>
+                            <div className="row border border-2 align-items-center rounded-4 my-5 p-3">
+                                <h6 className="col-lg-8 col-md-8 col-sm-8">
+                                    <a href={website.url}>{website.url}</a>
+                                </h6>
+                                <h6 className="col-lg-2 col-md-2 col-sm-2">
+                                    {website.credentials.length}
+                                </h6>
+                                <div className="col-lg-2 col-md-2 col-sm-2">
+                                    <a
+                                        className="btn btn-primary w-100"
+                                        data-bs-toggle="collapse"
+                                        href={`#c-${website.id}`}
+                                        role="button"
+                                        aria-expanded="false"
+                                        aria-controls={`c-${website.id}`}
+                                    >
+                                        View
+                                    </a>
+                                </div>
 
-                            {website.credentials && website.credentials.map(credential => (
-                                <div className="collapse mt-3" id={`c-${website.id}`}>
-                                    <div className="card card-body">
-                                        <div className="row">
-                                            <div className="col-lg-8 col-sm-12">
-                                                <p>username: <i><b>{credential.username}</b></i></p>
-                                                <p>password: <i><b>{credential.password}</b></i></p>
-                                            </div>
+                                {website.credentials && (
+                                    <div className="collapse mt-3" id={`c-${website.id}`}>
+                                        <div className="card card-body">
+                                            {website.credentials.map((credential) => (
+                                                <div className="row" key={credential.id}>
+                                                    <div className="col-lg-8 col-sm-12">
+                                                        <p>
+                                                            username: <i><b>{credential.username}</b></i>
+                                                        </p>
+                                                        <p>
+                                                            password: <i><b>{credential.password}</b></i>
+                                                        </p>
+                                                    </div>
 
-                                            <div className="col-lg-2 col-sm-12 my-2">
-                                                <a href={`/edit/${credential.id}/`} className="btn btn-outline-dark w-100">Edit</a>
-                                            </div>
+                                                    <div className="col-lg-2 col-sm-12 my-2">
+                                                        <a
+                                                            href={`/edit/${credential.id}/`}
+                                                            className="btn btn-outline-dark w-100"
+                                                        >
+                                                            Edit
+                                                        </a>
+                                                    </div>
 
-                                            <div className="col-lg-2 col-sm-12 my-2">
-                                                <a href={`/delete/${credential.id}/`} className="btn btn-outline-danger w-100">Delete</a>
-                                            </div>
+                                                    <div className="col-lg-2 col-sm-12 my-2">
+                                                        <a
+                                                            href={`/delete/${credential.id}/`}
+                                                            className="btn btn-outline-danger w-100"
+                                                        >
+                                                            Delete
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            ))}
                                         </div>
                                     </div>
-                                </div>
-                            ))}
+                                )}
+                            </div>
                         </div>
-                    </div>
-                ))}
-
-                {websites.length === 0 &&
+                    ))
+                ) : (
                     <div>
                         <img src="https://www.flaticon.com/free-icons/sad" />
                         <h5>You have no passwords. Create one.</h5>
                     </div>
-                }
+                )}
+
 
                 <a href="/create" className="btn btn-lg my-3 mx-auto add-password">Add Password +</a>
 
