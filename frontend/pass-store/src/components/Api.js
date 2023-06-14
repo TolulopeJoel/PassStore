adffc2a9ae8e2db46c83ae5a3dc29026b91ab11a
Tolu Joel <toluisjoel@gmail.com>
2023-06-14 11:26:53 +0100

    Updated api base url

--- frontend/pass-store/src/components/Api.js
+++ frontend/pass-store/src/components/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const apiBaseURL = "https://pass-store.up.railway.app/api/"
+const apiBaseURL = "https://pass-store.onrender.com/api/"
 
 const api = axios.create({
   baseURL: apiBaseURL,
